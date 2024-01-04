69c98113aa1b0328917824558d47205138ae93c7
Gorka Urzelai <80177434+gurzelai@users.noreply.github.com>
2024-01-04 17:46:35 +0100

    Add files via upload

--- multi-cronometro.py
+++ multi-cronometro.py
@@ -1,9 +1,8 @@
 import tkinter as tk
 import time
-import shelve
 from tkinter import messagebox
 
-def ventana_opciones():   
+def window():   
     screen_width = root.winfo_screenwidth()
     screen_height = root.winfo_screenheight()
 
@@ -13,31 +12,22 @@ def ventana_opciones():
     y = (screen_height/4) - (height/2)
     root.geometry("%dx%d+%d+%d" % (width, height, x, y))
 
-
 def on_closing():
     global running
-    running = False
-    with shelve.open("timer_data") as data:
-        data["elapsed_time"] = elapsed_time
-        data["name"] = name_entry.get()
-    
+    running = False    
     root.destroy()
 
 root = tk.Tk()
-root.title("Cronometro")
-root.protocol("WM_DELETE_WINDOW", on_closing)
+root.title("Timer")
 
-with shelve.open("timer_data") as data:
-    elapsed_time = data.get("elapsed_time", 0)
-    name = data.get("name", "")
+elapsed_time = 0
+name = ""
 
-ventana_opciones()
+window()
 
 def start_timer():
     global running, elapsed_time
     running = False
-    
-    print(elapsed_time)
     
     if not running:
         start_time = time.time() - elapsed_time
@@ -50,14 +40,14 @@ def start_timer():
         timer_label.config(text=formatted_time)
         root.update()
         
-start_button = tk.Button(root, text="Iniciar Cronómetro",  command=start_timer)
+start_button = tk.Button(root, text="Start timer",  command=start_timer)
 start_button.grid(row=0, column=0, padx=5, pady=5, ipadx=5, ipady=5)
 
 def stop_timer():
     global running
     running = False
     
-stop_button = tk.Button(root, text="Detener Cronómetro", command=stop_timer)
+stop_button = tk.Button(root, text="Stop timer", command=stop_timer)
 stop_button.grid(row=0, column=1, padx=5, pady=5, ipadx=5, ipady=5)
 
 formatted_time = time.strftime("%H:%M:%S", time.gmtime(elapsed_time))
@@ -66,18 +56,15 @@ timer_label.grid(row=0, column=2, padx=5, pady=5, ipadx=5, ipady=5)
 
 def reset_timer():
     global elapsed_time, running
-    if messagebox.askyesno("Reiniciar cronómetro", "¿Estás seguro de que deseas reiniciar el cronómetro?"):
+    if messagebox.askyesno("Restart timer", "Are you sure?"):
         running = False
         elapsed_time = 0
         timer_label.config(text="00:00:00")
 
-reset_button = tk.Button(root, text="Resetear Cronómetro", command=reset_timer)
+reset_button = tk.Button(root, text="Restart timer", command=reset_timer)
 reset_button.grid(row=0, column=3, padx=5, pady=5, ipadx=5, ipady=5)
 
 name_entry = tk.Entry(root)
 name_entry.grid(row=0, column=4, padx=5, pady=5, ipadx=5, ipady=5)
-name_entry.insert(0, name)
-
-
 
 root.mainloop()
